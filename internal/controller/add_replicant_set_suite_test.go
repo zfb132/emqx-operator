bca111788b6da95e4357f582629764061993cecc
Andrew Maiorov <andrew.mayorov@emqx.io>
2025-09-11 12:10:04 +0200

    test: simplify + make `addReplicantSet` suite more readable
    Signed-off-by: Andrew Maiorov <andrew.mayorov@emqx.io>

--- internal/controller/add_replicant_set_suite_test.go
+++ internal/controller/add_replicant_set_suite_test.go
@@ -10,12 +10,44 @@ import (
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	. "github.com/emqx/emqx-operator/test/utils"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Check add repl controller", Ordered, Label("repl"), func() {
-	var a *addRepl
+func actualInstance(instance *appsv2beta1.EMQX) *appsv2beta1.EMQX {
+	_ = k8sClient.Get(ctx, client.ObjectKeyFromObject(instance), instance)
+	return instance
+}
+
+func replicantSets(instance *appsv2beta1.EMQX) []appsv1.ReplicaSet {
+	list := &appsv1.ReplicaSetList{}
+	_ = k8sClient.List(ctx, list,
+		client.InNamespace(instance.Namespace),
+		client.MatchingLabels(appsv2beta1.DefaultReplicantLabels(instance)),
+	)
+	return list.Items
+}
+
+func adoptReplicantSet(instance *appsv2beta1.EMQX) *appsv1.ReplicaSet {
+	list := replicantSets(instance)
+	if len(list) == 0 {
+		return nil
+	}
+	rs := list[0].DeepCopy()
+	rsHash := rs.Labels[appsv2beta1.LabelsPodTemplateHashKey]
+	instance.Status.ReplicantNodesStatus.UpdateRevision = rsHash
+	return rs
+}
+
+func replicantSetsReconcileRound(instance *appsv2beta1.EMQX) *reconcileRound {
+	round := newReconcileRound()
+	round.state = loadReconcileState(ctx, k8sClient, instance)
+	return round
+}
+
+var _ = Describe("Reconciler addReplicantSet", Ordered, func() {
+	var a *addReplicantSet
 	var instance *appsv2beta1.EMQX = new(appsv2beta1.EMQX)
 	var ns *corev1.Namespace = &corev1.Namespace{}
 
@@ -62,189 +94,151 @@ var _ = Describe("Check add repl controller", Ordered, Label("repl"), func() {
 	})
 
 	Context("replicant template is nil", func() {
-		JustBeforeEach(func() {
-			instance.Spec.ReplicantTemplate = nil
-		})
-
 		It("should do nothing", func() {
-			Eventually(a.reconcile).WithArguments(newReconcileRound(), instance).
+			// Clear replicant template:
+			instance.Spec.ReplicantTemplate = nil
+			// Reconciliation step should do nothing and succeed:
+			round := replicantSetsReconcileRound(instance)
+			Eventually(a.reconcile).WithArguments(round, instance).
 				WithTimeout(timeout).
 				WithPolling(interval).
 				Should(Equal(subResult{}))
-			Eventually(func() []appsv1.ReplicaSet {
-				list := &appsv1.ReplicaSetList{}
-				_ = k8sClient.List(ctx, list,
-					client.InNamespace(instance.Namespace),
-					client.MatchingLabels(appsv2beta1.DefaultReplicantLabels(instance)),
-				)
-				return list.Items
-			}).Should(BeEmpty())
+			Eventually(replicantSets).WithArguments(instance).
+				Should(BeEmpty())
 		})
 	})
 
 	Context("core nodes is not ready", func() {
-		JustBeforeEach(func() {
-			instance.Status.RemoveCondition(appsv2beta1.CoreNodesReady)
-		})
-
 		It("should do nothing", func() {
-			Eventually(a.reconcile).WithArguments(newReconcileRound(), instance).
+			// Remove core nodes ready condition:
+			instance.Status.RemoveCondition(appsv2beta1.CoreNodesReady)
+			// Reconciliation step should succeed:
+			round := replicantSetsReconcileRound(instance)
+			Eventually(a.reconcile).WithArguments(round, instance).
 				WithTimeout(timeout).
 				WithPolling(interval).
 				Should(Equal(subResult{}))
-			Eventually(func() []appsv1.ReplicaSet {
-				list := &appsv1.ReplicaSetList{}
-				_ = k8sClient.List(ctx, list,
-					client.InNamespace(instance.Namespace),
-					client.MatchingLabels(appsv2beta1.DefaultReplicantLabels(instance)),
-				)
-				return list.Items
-			}).Should(BeEmpty())
+			Eventually(replicantSets).WithArguments(instance).
+				Should(BeEmpty())
 		})
 	})
 
 	Context("replicant template is not nil, and core code is ready", func() {
+
 		It("should create replicaSet", func() {
-			Eventually(a.reconcile).WithArguments(newReconcileRound(), instance).
+			round := replicantSetsReconcileRound(instance)
+			Eventually(a.reconcile).WithArguments(round, instance).
 				WithTimeout(timeout).
 				WithPolling(interval).
 				Should(Equal(subResult{}))
-			Eventually(func() []appsv1.ReplicaSet {
-				list := &appsv1.ReplicaSetList{}
-				_ = k8sClient.List(ctx, list,
-					client.InNamespace(instance.Namespace),
-					client.MatchingLabels(appsv2beta1.DefaultReplicantLabels(instance)),
-				)
-				return list.Items
-			}).Should(ConsistOf(
-				HaveField("Spec.Template.Spec.Containers", ConsistOf(
-					HaveField("Image", Equal(instance.Spec.Image)),
-				)),
-			))
+			Eventually(replicantSets).WithArguments(instance).
+				Should(ConsistOf(
+					HaveField("Spec.Template.Spec.Containers", ConsistOf(
+						HaveField("Image", Equal(instance.Spec.Image)),
+					)),
+				))
 		})
+
 	})
 
 	Context("scale down replicas count", func() {
+		BeforeAll(func() {
+			Eventually(adoptReplicantSet).WithArguments(instance).Should(Not(BeNil()))
+		})
+
 		JustBeforeEach(func() {
-			list := &appsv1.ReplicaSetList{}
-			Eventually(func() []appsv1.ReplicaSet {
-				_ = k8sClient.List(ctx, list,
-					client.InNamespace(instance.Namespace),
-					client.MatchingLabels(appsv2beta1.DefaultReplicantLabels(instance)),
-				)
-				return list.Items
-			}).Should(HaveLen(1))
-			rs := list.Items[0].DeepCopy()
+			rs := adoptReplicantSet(instance)
 			rs.Status.Replicas = 3
 			Expect(k8sClient.Status().Update(ctx, rs)).Should(Succeed())
 			Eventually(func() *appsv1.ReplicaSet {
 				_ = k8sClient.Get(ctx, client.ObjectKeyFromObject(rs), rs)
 				return rs
 			}).WithTimeout(timeout).WithPolling(interval).Should(
-				WithTransform(func(s *appsv1.ReplicaSet) int32 { return s.Status.Replicas }, Equal(int32(3))),
+				HaveField("Status.Replicas", BeEquivalentTo(3)),
 			)
-
-			instance.Status.ReplicantNodesStatus.UpdateRevision = rs.Labels[appsv2beta1.LabelsPodTemplateHashKey]
-			instance.Spec.ReplicantTemplate.Spec.Replicas = ptr.To(int32(0))
 		})
 
 		It("should update replicaSet", func() {
-			Eventually(a.reconcile).WithArguments(newReconcileRound(), instance).
+			// Set replicas count to 0:
+			instance.Spec.ReplicantTemplate.Spec.Replicas = ptr.To(int32(0))
+			// Reconciliation step should succeed:
+			round := replicantSetsReconcileRound(instance)
+			Eventually(a.reconcile).WithArguments(round, instance).
 				WithTimeout(timeout).
 				WithPolling(interval).
 				Should(Equal(subResult{}))
-			Eventually(func() []appsv1.ReplicaSet {
-				list := &appsv1.ReplicaSetList{}
-				_ = k8sClient.List(ctx, list,
-					client.InNamespace(instance.Namespace),
-					client.MatchingLabels(appsv2beta1.DefaultReplicantLabels(instance)),
-				)
-				return list.Items
-			}).Should(ConsistOf(
-				HaveField("Spec.Replicas", Equal(*instance.Spec.ReplicantTemplate.Spec.Replicas)),
-			))
+			// ReplicaSet should be updated in place:
+			Eventually(replicantSets).WithArguments(instance).
+				Should(ConsistOf(
+					HaveField("Spec.Replicas", HaveValue(BeEquivalentTo(0))),
+				))
 		})
+
+		AfterAll(func() {
+			Eventually(adoptReplicantSet).WithArguments(instance).Should(Not(BeNil()))
+		})
+
 	})
 
 	Context("scale up replicas count", func() {
-		JustBeforeEach(func() {
-			list := &appsv1.ReplicaSetList{}
-			Eventually(func() []appsv1.ReplicaSet {
-				_ = k8sClient.List(ctx, list,
-					client.InNamespace(instance.Namespace),
-					client.MatchingLabels(appsv2beta1.DefaultReplicantLabels(instance)),
-				)
-				return list.Items
-			}).Should(HaveLen(1))
-			instance.Status.ReplicantNodesStatus.UpdateRevision = list.Items[0].Labels[appsv2beta1.LabelsPodTemplateHashKey]
-
-			instance.Spec.ReplicantTemplate.Spec.Replicas = ptr.To(int32(4))
+		BeforeAll(func() {
+			Eventually(adoptReplicantSet).WithArguments(instance).Should(Not(BeNil()))
 		})
 
 		It("should update replicaSet", func() {
-			Eventually(a.reconcile).WithArguments(newReconcileRound(), instance).
+			// Set replicas count to 4:
+			instance.Spec.ReplicantTemplate.Spec.Replicas = ptr.To(int32(4))
+			// Reconciliation step should succeed:
+			round := replicantSetsReconcileRound(instance)
+			Eventually(a.reconcile).WithArguments(round, instance).
 				WithTimeout(timeout).
 				WithPolling(interval).
 				Should(Equal(subResult{}))
-			Eventually(func() []appsv1.ReplicaSet {
-				list := &appsv1.ReplicaSetList{}
-				_ = k8sClient.List(ctx, list,
-					client.InNamespace(instance.Namespace),
-					client.MatchingLabels(appsv2beta1.DefaultReplicantLabels(instance)),
-				)
-				return list.Items
-			}).Should(ConsistOf(
-				WithTransform(func(rs appsv1.ReplicaSet) int32 { return *rs.Spec.Replicas }, Equal(*instance.Spec.ReplicantTemplate.Spec.Replicas)),
-			))
-
-			Eventually(func() *appsv2beta1.EMQX {
-				_ = k8sClient.Get(ctx, client.ObjectKeyFromObject(instance), instance)
-				return instance
-			}).Should(And(
-				WithTransform(func(emqx *appsv2beta1.EMQX) string { return emqx.Status.GetLastTrueCondition().Type }, Equal(appsv2beta1.ReplicantNodesProgressing)),
-				WithTransform(func(emqx *appsv2beta1.EMQX) bool { return emqx.Status.IsConditionTrue(appsv2beta1.Ready) }, BeFalse()),
-				WithTransform(func(emqx *appsv2beta1.EMQX) bool { return emqx.Status.IsConditionTrue(appsv2beta1.Available) }, BeFalse()),
-				WithTransform(func(emqx *appsv2beta1.EMQX) bool {
-					return emqx.Status.IsConditionTrue(appsv2beta1.ReplicantNodesReady)
-				}, BeFalse()),
-			))
+			// ReplicaSet should be updated:
+			Eventually(replicantSets).WithArguments(instance).
+				Should(ConsistOf(
+					HaveField("Spec.Replicas", HaveValue(BeEquivalentTo(4))),
+				))
+			// Status conditions should reset to `ReplicantNodesProgressing`:
+			Eventually(actualInstance).WithArguments(instance).
+				Should(And(
+					WithTransform(func(emqx *appsv2beta1.EMQX) string { return emqx.Status.GetLastTrueCondition().Type }, Equal(appsv2beta1.ReplicantNodesProgressing)),
+					HaveCondition(appsv2beta1.Ready, BeNil()),
+					HaveCondition(appsv2beta1.Available, BeNil()),
+					HaveCondition(appsv2beta1.ReplicantNodesReady, BeNil()),
+				))
 		})
 	})
 
 	Context("change image", func() {
-		JustBeforeEach(func() {
-			instance.Spec.Image = "emqx/emqx"
-			instance.Spec.UpdateStrategy.InitialDelaySeconds = int32(999999999)
+		BeforeAll(func() {
+			Eventually(adoptReplicantSet).WithArguments(instance).Should(Not(BeNil()))
 		})
 
 		It("should create new replicaSet", func() {
-			Eventually(a.reconcile).WithArguments(newReconcileRound(), instance).
+			// Introduce changes that require creating a new replicaSet:
+			instance.Spec.Image = "emqx/emqx"
+			instance.Spec.UpdateStrategy.InitialDelaySeconds = int32(999999999)
+			// Reconciliation step should succeed:
+			round := replicantSetsReconcileRound(instance)
+			Eventually(a.reconcile).WithArguments(round, instance).
 				WithTimeout(timeout).
 				WithPolling(interval).
 				Should(Equal(subResult{}))
-			Eventually(func() []appsv1.ReplicaSet {
-				list := &appsv1.ReplicaSetList{}
-				_ = k8sClient.List(ctx, list,
-					client.InNamespace(instance.Namespace),
-					client.MatchingLabels(appsv2beta1.DefaultReplicantLabels(instance)),
-				)
-				return list.Items
-			}).Should(ConsistOf(
-				WithTransform(func(rs appsv1.ReplicaSet) string { return rs.Spec.Template.Spec.Containers[0].Image }, Equal(emqx.Spec.Image)),
-				WithTransform(func(rs appsv1.ReplicaSet) string { return rs.Spec.Template.Spec.Containers[0].Image }, Equal(instance.Spec.Image)),
-			))
-
-			Eventually(func() *appsv2beta1.EMQX {
-				_ = k8sClient.Get(ctx, client.ObjectKeyFromObject(instance), instance)
-				return instance
-			}).Should(And(
-				WithTransform(func(emqx *appsv2beta1.EMQX) string { return emqx.Status.GetLastTrueCondition().Type }, Equal(appsv2beta1.ReplicantNodesProgressing)),
-				WithTransform(func(emqx *appsv2beta1.EMQX) bool { return emqx.Status.IsConditionTrue(appsv2beta1.Ready) }, BeFalse()),
-				WithTransform(func(emqx *appsv2beta1.EMQX) bool { return emqx.Status.IsConditionTrue(appsv2beta1.Available) }, BeFalse()),
-				WithTransform(func(emqx *appsv2beta1.EMQX) bool {
-					return emqx.Status.IsConditionTrue(appsv2beta1.ReplicantNodesReady)
-				}, BeFalse()),
-			))
+			// There should be two replicaSets soon:
+			Eventually(replicantSets).WithArguments(instance).
+				Should(ConsistOf(
+					HaveField("Spec.Template.Spec.Containers", ConsistOf(HaveField("Image", Equal(emqx.Spec.Image)))),
+					HaveField("Spec.Template.Spec.Containers", ConsistOf(HaveField("Image", Equal(instance.Spec.Image)))),
+				))
+			// Status conditions should reset to `ReplicantNodesProgressing`:
+			Eventually(actualInstance).WithArguments(instance).
+				Should(And(
+					WithTransform(func(emqx *appsv2beta1.EMQX) string { return emqx.Status.GetLastTrueCondition().Type }, Equal(appsv2beta1.ReplicantNodesProgressing)),
+					HaveCondition(appsv2beta1.Ready, BeNil()),
+					HaveCondition(appsv2beta1.Available, BeNil()),
+					HaveCondition(appsv2beta1.ReplicantNodesReady, BeNil()),
+				))
 		})
 	})
 
